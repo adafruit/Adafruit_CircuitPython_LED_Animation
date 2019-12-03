1a673ec155fa89693fa1f60a664cfd63be6e264f
Kattni Rembor <kattni@adafruit.com>
2019-12-03 16:24:50 -0500

    Update docstring in example.

--- examples/led_animation_simpletest.py
+++ examples/led_animation_simpletest.py
@@ -5,7 +5,7 @@ from adafruit_led_animation.animation import Blink
 import adafruit_led_animation.color as color
 
 # Works on Circuit Playground Express and Bluefruit.
-# For other boards, change board.NEOPIXEL to match the pin to which you attached your NeoPixels.
+# For other boards, change board.NEOPIXEL to match the pin to which the NeoPixels are attached.
 pixel_pin = board.NEOPIXEL
 # Change to match the number of pixels you have attached to your board.
 num_pixels = 10
