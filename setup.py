ebefc3075411736806bbaa16969e38d80e866095
Kattni Rembor <kattni@adafruit.com>
2020-04-10 13:57:55 -0400

    Black reformatting with Python 3 target.

--- setup.py
+++ setup.py
@@ -29,9 +29,7 @@ setup(
     # Author details
     author="Adafruit Industries",
     author_email="circuitpython@adafruit.com",
-    install_requires=[
-        "Adafruit-Blinka",
-    ],
+    install_requires=["Adafruit-Blinka",],
     # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
