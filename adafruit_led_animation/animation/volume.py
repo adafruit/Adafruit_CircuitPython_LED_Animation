eb86bf0b4eb038a4c1d1a47bb171af4099d569e2
gamblor21 <mark.komus@gmail.com>
2020-09-28 16:32:30 -0500

    Fixed errors from pylint and black

--- adafruit_led_animation/animation/volume.py
+++ adafruit_led_animation/animation/volume.py
@@ -34,6 +34,20 @@ Implementation Notes
 
 from adafruit_led_animation.animation import Animation
 
+
+def map_range(x, in_min, in_max, out_min, out_max):
+    """
+    Maps a number from one range to another.
+    :return: Returns value mapped to new range
+    :rtype: float
+    """
+    mapped = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    if out_min <= out_max:
+        return max(min(mapped, out_max), out_min)
+
+    return min(max(mapped, out_max), out_min)
+
+
 class Volume(Animation):
     """
     Animate the brightness and number of pixels based on volume.
@@ -44,31 +58,59 @@ class Volume(Animation):
     :param float max_volume: what volume is considered maximum where everything is lit up
     """
 
-    def __init__(self, pixel_object, speed, brightest_color, decoder, max_volume=500, name=None):
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self, pixel_object, speed, brightest_color, decoder, max_volume=500, name=None
+    ):
         self._decoder = decoder
         self._num_pixels = len(pixel_object)
         self._max_volume = max_volume
-        self._brigthest_color = brightest_color
+        self._brightest_color = brightest_color
         super().__init__(pixel_object, speed, brightest_color, name=name)
 
-    def _set_color(self, brightest_color):
-        self.colors = [brightest_color]
-
-    def map_range(self, x, in_min, in_max, out_min, out_max):
-        mapped = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-        if out_min <= out_max:
-            return max(min(mapped, out_max), out_min)
-
-        return min(max(mapped, out_max), out_min)
+    def set_brightest_color(self, brightest_color):
+        """
+        Animate the brightness and number of pixels based on volume.
+        :param brightest_color: Color at max volume ``(r, g, b)`` tuple, or ``0x000000`` hex format
+        """
+        self._brightest_color = brightest_color
 
     def draw(self):
-        red = int(self.map_range(self._decoder.rms_level, 0, self._max_volume, 0, self._brigthest_color[0]))
-        green = int(self.map_range(self._decoder.rms_level, 0, self._max_volume, 0, self._brigthest_color[1]))
-        blue = int(self.map_range(self._decoder.rms_level, 0, self._max_volume, 0, self._brigthest_color[2]))
+        red = int(
+            map_range(
+                self._decoder.rms_level,
+                0,
+                self._max_volume,
+                0,
+                self._brightest_color[0],
+            )
+        )
+        green = int(
+            map_range(
+                self._decoder.rms_level,
+                0,
+                self._max_volume,
+                0,
+                self._brightest_color[1],
+            )
+        )
+        blue = int(
+            map_range(
+                self._decoder.rms_level,
+                0,
+                self._max_volume,
+                0,
+                self._brightest_color[2],
+            )
+        )
 
-        lit_pixels = int(self.map_range(self._decoder.rms_level, 0, self._max_volume, 0, self._num_pixels))
+        lit_pixels = int(
+            map_range(self._decoder.rms_level, 0, self._max_volume, 0, self._num_pixels)
+        )
         if lit_pixels > self._num_pixels:
             lit_pixels = self._num_pixels
 
-        self.pixel_object[0:lit_pixels] = [(red,green,blue)] * lit_pixels
-        self.pixel_object[lit_pixels:self._num_pixels] = [(0,0,0)] * (self._num_pixels-lit_pixels)
+        self.pixel_object[0:lit_pixels] = [(red, green, blue)] * lit_pixels
+        self.pixel_object[lit_pixels : self._num_pixels] = [(0, 0, 0)] * (
+            self._num_pixels - lit_pixels
+        )
