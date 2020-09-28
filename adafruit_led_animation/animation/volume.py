d12b7fb50b74aabfc603b710a39bac6e78fa5533
gamblor21 <mark.komus@gmail.com>
2020-09-28 16:03:36 -0500

    Added license and documentation

--- adafruit_led_animation/animation/volume.py
+++ adafruit_led_animation/animation/volume.py
@@ -1,3 +1,37 @@
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`adafruit_led_animation.animation.volume`
+================================================================================
+Volume animation for CircuitPython helper library for LED animations.
+* Author(s): Mark Komus
+Implementation Notes
+--------------------
+**Hardware:**
+* `Adafruit NeoPixels <https://www.adafruit.com/category/168>`_
+* `Adafruit DotStars <https://www.adafruit.com/category/885>`_
+**Software and Dependencies:**
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+"""
+
 from adafruit_led_animation.animation import Animation
 
 class Volume(Animation):
@@ -6,6 +40,8 @@ class Volume(Animation):
     :param pixel_object: The initialised LED object.
     :param float speed: Animation update speed in seconds, e.g. ``0.1``.
     :param brightest_color: Color at max volume ``(r, g, b)`` tuple, or ``0x000000`` hex format
+    :param decoder: a MP3Decoder object that the volume will be taken from
+    :param float max_volume: what volume is considered maximum where everything is lit up
     """
 
     def __init__(self, pixel_object, speed, brightest_color, decoder, max_volume=500, name=None):
