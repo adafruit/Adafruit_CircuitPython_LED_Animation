e296e6f67a3689b28a6d65d4c48a139e1398faba
Kattni Rembor <kattni@adafruit.com>
2021-12-21 11:17:31 -0500

    Remove specific Python, use 3.x

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ autodoc_mock_imports = ["rainbowio"]
 
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.4", None),
+    "python": ("https://docs.python.org/3", None),
     "CircuitPython": ("https://circuitpython.readthedocs.io/en/latest/", None),
 }
 
